83e792f9bdba6ba692626051d93f6a0b5d3fbb5c
RAdKuzur <drive16052003@gmail.com>
2025-05-07 11:41:41 +0400

    fix

--- resources/views/defence/leaderboard.blade.php
+++ resources/views/defence/leaderboard.blade.php
@@ -7,7 +7,7 @@
         <h1>Таблица: {{ $defence->name }}</h1>
         <div class="container mt-5">
             <div id="score-table">
-                @include('defence.leaderboard-table', ['acts' => $acts, 'defence' => $defence])
+                @include('defence.leaderboard-update', ['acts' => $acts, 'defence' => $defence])
             </div>
         </div>
         <a href="{{ route('defence.show', $defence->id) }}" class="btn btn-secondary mt-3">Назад к защите</a>
